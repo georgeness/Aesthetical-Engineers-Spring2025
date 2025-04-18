aae8a3227a50d6a7d09452b27ae1119ee7e60167
yvoryk <yvoryk@ufl.edu>
2025-04-18 09:43:01 -0400

    ,

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Only use static export for production builds on GitHub Pages
-  ...(process.env.GITHUB_ACTIONS && {
-    output: 'export',
-    basePath: '/Aesthetical-Engineers-Spring2025',
     images: {
-      unoptimized: true,
+      remotePatterns: [
+        {
+          protocol: 'https',
+          hostname: 'bhvqdgbwuom91t7g.public.blob.vercel-storage.com',
+          pathname: '**',
+        },
+      ],
     },
-  }),
-};
-
-export default nextConfig;
+    // Only use static export for production builds on GitHub Pages
+    ...(process.env.GITHUB_ACTIONS && {
+      output: 'export',
+      basePath: '/Aesthetical-Engineers-Spring2025',
+      images: {
+        unoptimized: true,
+      },
+    }),
+  };
+  
+  export default nextConfig;
